7eebd0ba09b4acc107913fbaeef68771ee3b0e63
Wonjun <wjjgjk@naver.com>
2024-12-01 00:16:08 -0600

    FIX: a typo

--- p3/__main__.py
+++ p3/__main__.py
@@ -5,7 +5,7 @@ Professor: Dr. Marnim Galib
 
 Wonjun Park
 UTA ID: 1002237177
-Computer Science and Engineering, University of Texs at Arlington, Arlington, TX, USA
+Computer Science and Engineering, University of Texas at Arlington, Arlington, TX, USA
 wxp7177@mavs.uta.edu
 """
 
